62fdb4ca4382fc0a15fb99e94ac7302b20bf2af0
Corbin Bremmeyr <bremmeyc@mail.gvsu.edu>
2021-12-03 22:46:37 -0600

    Finished w/ Day 2, only a day late....

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,11 @@ module Main where
 import System.IO
 import Control.Monad
 
-f :: [String] -> [Int]
-f = map read
+f :: [String] -> [(String, Int)]
+f xs = map mapper xs
+     where 
+        mapper x = (head x_split, read (last x_split) :: Int)
+                   where x_split = words x
 
 main = do
 
@@ -17,21 +20,53 @@ main = do
     let input = []
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
-    let singlewords = words contents
-        input = f singlewords
+    let singlelines = lines contents
+        input = f singlelines
     -- print input
 
     -- Day 1.1
-    let result = countTrues (isBigger input)
-    print result
+    -- let result = countTrues (isBigger input)
+    -- print result
 
     -- Day 1.2
-    let result = countTrues (isBigger (slidingSum 3 input))
-    print result
+    -- let result = countTrues (isBigger (slidingSum 3 input))
+    -- print result
+
+    -- Day 2.1
+    -- let forward_sum = sumDirs "forward" input
+    -- let down_sum    = sumDirs "down" input
+    -- let up_sum      = sumDirs "up" input
+
+    -- print (forward_sum * (down_sum - up_sum))
+
+    -- Day 2.2
+    let pos = get_pos input
+    print pos
+    print ((fst pos) * (snd pos))
 
     -- Clean up stuff
     hClose handle
 
+-- Day 2 stuff
+sumDirs :: String -> [(String, Int)] -> Int
+sumDirs dir xs = sum (map snd dir_matches)
+                 where dir_matches = (filter ((== dir).fst) xs)
+
+get_pos :: [(String, Int)] -> (Int, Int)    -- (h_pos, depth, aim)
+get_pos xs = (fst3 final_pos, snd3 final_pos)
+             where final_pos = foldl pos_step (0, 0, 0) xs
+
+--          Move to apply    Current position   New position
+pos_step :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
+pos_step (h_pos, depth, aim) (dir, delta)
+    | dir == "forward" = (h_pos + delta,            -- new h_pos
+                          depth + (aim * delta),    -- new depth
+                          aim)                      -- aim no change
+
+    | dir == "down" = (h_pos, depth, aim + delta)   -- increase aim
+    | dir == "up"   = (h_pos, depth, aim - delta)   -- decrease aim
+
+-- Day 1 stuff
 isBigger :: [Int] -> [Bool]
 isBigger [] = []
 isBigger (x:[]) = []
@@ -42,6 +77,16 @@ countTrues xs = sum (map (\x -> if x then 1 else 0) xs)
 
 slidingSum :: Int -> [Int] -> [Int]
 slidingSum w xs = if length window == w then
-                    (sum (take w xs)):(slidingSum w (tail xs))
+                    (sum window):(slidingSum w (tail xs))
                     else []
                 where window = (take w xs)
+
+-- Helper functions
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, x) = x
